3a890f019b522cebd690218ce0fdfe8b16a3e3f4
moyudaiwang <ch8bw8k4p8>
2021-04-22 21:35:48 +0800

    donQuixotey-hydrogen-1.1.0 2021-04-22 23:15
    2:HandCreateTable本地自动生成建表SQL工具方法

--- src/main/java/com/element/hydrogen/utils/dev/HandCreateTable.java
+++ src/main/java/com/element/hydrogen/utils/dev/HandCreateTable.java
@@ -1,37 +1,56 @@
-package com.element.hydrogen.utils.factory;
+package com.element.hydrogen.utils.dev;
+
 import jxl.Sheet;
 import jxl.Workbook;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Date;
 
 /**
- * @ClassName CreateTable
+ * @ClassName HandCreateTable
  * @Description 建表工具：导入Excel生成mysql建表文件
+ * 1、生成备份原有表数据的SQL：DROP TABLE DON_USER_INFO_BAK;
+ * CREATE TABLE DON_USER_INFO_BAK AS SELECT * FROM DON_USER_INFO;
+ * 2:生成delete表结构语句；
+ * 3：生成新的建表语句；
  * @Author yanyu
- * @Date 2021/2/27 23:25
+ * @Date 2021/4/22 21:30
  * @Version 1.0
  */
-public class CreateTable {
+public class HandCreateTable {
+
+    private  static final  String EXCEL_PATH ="C:\\Users\\yanyu\\Desktop\\唐吉坷德文档库\\4_Tool\\CreateTable\\TableFile.xls";  //要读取的Excel路径
+    private  static final  String SQL_PATH ="C:\\Users\\yanyu\\Desktop\\唐吉坷德文档库\\4_Tool\\CreateTable\\result.sql";  //要生成的建表SQL的路径
+
+    private  static final  int LOOP_NUM =10;  //循环几个工作表（sheet）
+
     public static void main(String[] args) throws Exception {
-        File file = new File("C:\\Users\\yanyu\\Desktop\\唐吉坷德文档库\\4_Tool\\CreateTable\\TableFile.xls");
+        File file = new File(EXCEL_PATH);
         Workbook wb = Workbook.getWorkbook(file);
 
         Sheet[] sheets = wb.getSheets();
         System.out.println("当前文件夹的sheet数量" + sheets.length);
         Date dateStart = new Date();
-        PrintWriter writer = new PrintWriter("C:\\Users\\yanyu\\Desktop\\唐吉坷德文档库\\4_Tool\\CreateTable\\result.sql") ;
+        PrintWriter writer = new PrintWriter(SQL_PATH) ;
+        //循环生成备份表SQL
+        writer.write("-- bak Table DDL:\r\n");
+        for (int i = 0; i < LOOP_NUM; i++) {
+            Sheet sheet = wb.getSheet(i);
+            String bakStr = bakTale(sheet);
+            writer.write(bakStr);
+        }
         //循环生成删除
         writer.write("-- delete Table DDL:\r\n");
-        for (int i = 0; i < sheets.length; i++) {
+        for (int i = 0; i < LOOP_NUM; i++) {
             Sheet sheet = wb.getSheet(i);
             String deleteStr = deleteTale(sheet);
             writer.write(deleteStr);
         }
         writer.write("\r\n");
         //循环生成建表
-        for (int i = 0; i < sheets.length; i++) {
+        for (int i = 0; i < LOOP_NUM; i++) {
             Sheet sheet = wb.getSheet(i);
             String createStr = createstatement(sheet);
             writer.write(createStr);
@@ -41,11 +60,19 @@ public class CreateTable {
         System.out.println("生成完毕，一共用时："+(dateEnd.getTime()-dateStart.getTime())/1000+"秒");
     }
 
+    //生成备份原有表数据ddl
+    private static String bakTale(Sheet sheet) {
+        StringBuffer bakStr = new StringBuffer();
+        bakStr.append("DROP TABLE "+sheet.getCell(0, 0).getContents()+ "_BAK;\r\n");
+        bakStr.append("CREATE TABLE "+sheet.getCell(0, 0).getContents()+ "_BAK AS SELECT * FROM "+sheet.getCell(0, 0).getContents() + ";\r\n");
+        return bakStr.toString();
+    }
+
     //生成删除表ddl
     private static String deleteTale(Sheet sheet) {
         StringBuffer delStr = new StringBuffer();
         //delStr.append("-- SELECT * FROM "+sheet.getCell(0, 0).getContents()+ ";\r\n");
-        delStr.append("DELETE TABLE "+sheet.getCell(0, 0).getContents()+ ";\r\n");
+        //delStr.append("DELETE TABLE "+sheet.getCell(0, 0).getContents()+ ";\r\n");
         return delStr.toString();
     }
 
@@ -57,7 +84,7 @@ public class CreateTable {
         StringBuffer partitionsb = new StringBuffer(); // 分区字段部分
         bodysb.append("-- "+sheet.getCell(0, 0).getContents()+" DDL：\r\n");
         //bodysb.append("-- SELECT * FROM "+sheet.getCell(0, 0).getContents()+ ";\r\n");
-        //bodysb.append("DELETE TABLE "+sheet.getCell(0, 0).getContents()+ ";\r\n");
+        bodysb.append("DELETE TABLE "+sheet.getCell(0, 0).getContents()+ ";\r\n");
         bodysb.append("CREATE TABLE "+sheet.getCell(0, 0).getContents());
         // 表头部分
         bodysb.append(" (\r\n");
