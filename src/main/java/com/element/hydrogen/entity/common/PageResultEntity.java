b6899b4659267de7c1f1668bd35e7dfd33e6690e
moyudaiwang <ch8bw8k4p8>
2022-02-03 03:52:38 +0800

    donQuixotey-hydrogen-1.1.0 2022-02-03 03:41
    1.1.0版本定板

--- src/main/java/com/element/hydrogen/entity/common/PageResultEntity.java
+++ src/main/java/com/element/hydrogen/entity/common/PageResultEntity.java
@@ -1,5 +1,7 @@
 package com.element.hydrogen.entity.common;
 
+import lombok.Data;
+
 import java.util.List;
 
 /**
@@ -9,6 +11,7 @@ import java.util.List;
  * @Date 2020/8/30 2:24
  * @Version 1.0
  */
+@Data
 public class PageResultEntity {
     /**
      * 当前页码
@@ -39,46 +42,9 @@ public class PageResultEntity {
      */
     private String operatorDate;
 
-    public int getPageNum() {
-        return pageNum;
-    }
-    public void setPageNum(int pageNum) {
-        this.pageNum = pageNum;
-    }
-    public int getPageSize() {
-        return pageSize;
-    }
-    public void setPageSize(int pageSize) {
-        this.pageSize = pageSize;
-    }
-    public long getTotalSize() {
-        return totalSize;
-    }
-    public void setTotalSize(long totalSize) {
-        this.totalSize = totalSize;
-    }
-    public int getTotalPages() {
-        return totalPages;
-    }
-    public void setTotalPages(int totalPages) {
-        this.totalPages = totalPages;
-    }
-    public List<?> getContent() {
-        return content;
-    }
-    public void setContent(List<?> content) {
-        this.content = content;
-    }
-    public String getOperator() {
-        return operator;
-    }
-    public void setOperator(String operator) {
-        this.operator = operator;
-    }
-    public String getOperatorDate() {
-        return operatorDate;
-    }
-    public void setOperatorDate(String operatorDate) {
-        this.operatorDate = operatorDate;
-    }
+    /**
+     * 查询参数
+     */
+    private String queryName;
+
 }
