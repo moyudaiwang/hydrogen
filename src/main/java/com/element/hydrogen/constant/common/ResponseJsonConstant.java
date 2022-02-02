9102ac485743587ee99475e284734717fba41947
moyudaiwang <ch8bw8k4p8>
2022-02-03 04:00:20 +0800

    donQuixotey-hydrogen-1.1.0 2022-02-03 03:41
    1.1.0版本定板

--- src/main/java/com/element/hydrogen/constant/common/ResponseJsonConstant.java
+++ src/main/java/com/element/hydrogen/constant/common/ResponseJsonConstant.java
@@ -1,25 +1,17 @@
 package com.element.hydrogen.constant.common;
 
 /**
- *
+ *常用返回信息
  * @author yanyu
  */
 public class ResponseJsonConstant {
 
-    public static final int SUCCESS = 200;
-    public static final int FAIL = 500;
-
+    public static final String SUCCESS = "100200";
     public static final String SUC_QUE = "查询成功";
     public static final String SUC_ADD = "新增成功";
     public static final String SUC_UPD = "修改成功";
     public static final String SUC_DEL = "删除成功";
     public static final String SUC_UPL = "导入成功";
-
-    public static final String FAIL_QUE = "查询失败";
-    public static final String FAIL_ADD = "新增失败";
-    public static final String FAIL_UPD = "修改失败";
-    public static final String FAIL_DEL = "删除失败";
-    public static final String FAIL_UPL = "导入失败";
 
     public static final String EXI_QUE_OK = "已存在，查询成功";
     public static final String EXI_ADD_OK = "已存在，新增成功";
@@ -27,17 +19,24 @@ public class ResponseJsonConstant {
     public static final String EXI_DEL_OK = "已存在，删除成功";
     public static final String EXI_UPL_OK = "已存在，导入成功";
 
-    public static final String EXI_QUE_NO = "已存在，查询失败";
-    public static final String EXI_ADD_NO = "已存在，新增失败";
-    public static final String EXI_UPD_NO = "已存在，修改失败";
-    public static final String EXI_DEL_NO = "已存在，删除失败";
-    public static final String EXI_UPL_NO = "已存在，导入失败";
-
     public static final String RUN_QUE_OK = "运行中，查询成功";
     public static final String RUN_ADD_OK = "运行中，新增成功";
     public static final String RUN_UPD_OK = "运行中，修改成功";
     public static final String RUN_DEL_OK = "运行中，删除成功";
     public static final String RUN_UPL_OK = "运行中，导入成功";
+
+    public static final String FAIL = "500";
+    public static final String FAIL_QUE = "查询失败";
+    public static final String FAIL_ADD = "新增失败";
+    public static final String FAIL_UPD = "修改失败";
+    public static final String FAIL_DEL = "删除失败";
+    public static final String FAIL_UPL = "导入失败";
+
+    public static final String EXI_QUE_NO = "已存在，查询失败";
+    public static final String EXI_ADD_NO = "已存在，新增失败";
+    public static final String EXI_UPD_NO = "已存在，修改失败";
+    public static final String EXI_DEL_NO = "已存在，删除失败";
+    public static final String EXI_UPL_NO = "已存在，导入失败";
 
     public static final String RUN_QUE_NO = "运行中，查询失败";
     public static final String RUN_ADD_NO = "运行中，新增失败";
